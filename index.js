f31e9765843ec22dff6df92b7f538a913df8c67e
why <why@geekniu.com>
2018-02-02 11:01:37 +0800

    I modified the directory of project and provided some primary files compressed.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var html2markdown = require('./html2markdown');
-var htmlParser = require('./markdown_html_parser');
+var html2markdown = require('./dist/html2markdown');
+var htmlParser = require('./dist/markdown_html_parser');
 
 module.exports = function(html, opts) {
   opts = opts || {};
